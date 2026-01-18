17c00c8afc87b48dc75503621c7f565612457739
Solomon Adeniran <soluwaseun806@gmail.com>
2026-01-18 16:21:51 +0100

    grades count

--- day1.py
+++ day1.py
@@ -1,11 +1,27 @@
-numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-def find_even_numbers(numbers):
-     "Return a list of even numbers"
-     even_numbers = []
-     for number in numbers:
-          if number % 2 == 0:
-               even_numbers.append(number)
-     return even_numbers
+# def find_even_numbers(numbers):
+#      "Return a list of even numbers"
+#      even_numbers = []
+#      for number in numbers:
+#           if number % 2 == 0:
+#                even_numbers.append(number)
+#      return even_numbers
 
-print(find_even_numbers(numbers))
+# print(find_even_numbers(numbers))
+
+family = {'father': 'John','mother': 'Jane','son': 'Mike'}
+
+grades = ['A', 'B', 'A', 'C', 'B', 'A', 'D', 'C', 'B', 'A']
+def count_grades(grades):
+    "Return a dictionary with the count of each grade"
+    grade_count = {}
+
+    for grade in grades:
+        if grade in grade_count:
+            grade_count[grade] += 1
+        else:
+            grade_count[grade] = 1
+    return grade_count
+
+print(count_grades(grades))
