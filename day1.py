1acbdd2f20370aed114119d893c09e4d32bee389
Solomon Adeniran <soluwaseun806@gmail.com>
2026-01-18 16:01:29 +0100

    even

--- day1.py
+++ day1.py
@@ -1,2 +1,11 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 def find_even_numbers(numbers):
-     pass
+     "Return a list of even numbers"
+     even_numbers = []
+     for number in numbers:
+          if number % 2 == 0:
+               even_numbers.append(number)
+     return even_numbers
+
+print(find_even_numbers(numbers))
