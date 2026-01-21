f19e6d74bfbaf8bbd6bb31a719c97b718d8e3742
Solomon Adeniran <soluwaseun806@gmail.com>
2026-01-21 23:58:29 +0100

    loading sample data

--- day1.py
+++ day1.py
@@ -1,38 +1,14 @@
-# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+import numpy as np 
+import pandas as pd 
 
-# def find_even_numbers(numbers):
-#      "Return a list of even numbers"
-#      even_numbers = []
-#      for number in numbers:
-#           if number % 2 == 0:
-#                even_numbers.append(number)
-#      return even_numbers
+sample_data = {
+     'Crop_name': ['Wheat', 'Rice', 'Maize', 'Barley', 'Soybean'],
+     'Disease' : ['Rust', 'Blast', 'Smut', 'Powdery Mildew', 'Root Rot'],
+     'confidence_score': [0.95, 0.89, 0.92, 0.85, 0.90],
+     'region' : ['Lagos', 'Nairobi', 'Cairo', 'Johannesburg', 'Accra']
+}
 
-# print(find_even_numbers(numbers))
-
-# family = {'father': 'John','mother': 'Jane','son': 'Mike'}
-
-# grades = ['A', 'B', 'A', 'C', 'B', 'A', 'D', 'C', 'B', 'A']
-# def count_grades(grades):
-#     "Return a dictionary with the count of each grade"
-#     grade_count = {}
-
-#     for grade in grades:
-#         if grade in grade_count:
-#             grade_count[grade] += 1
-#         else:
-#             grade_count[grade] = 1
-#     return grade_count
-
-# print(count_grades(grades))
-
-from tkinter.font import names
-
-
-def student_summary(names,scores):
-    "Returns a dictionary of student summaries"
-    summary = {}
-    for name, score in zip(names, scores):
-        summary[name] = score
-    return summary
-print(student_summary(['Alice', 'Bob', 'Charlie'], [85, 92, 78]))
+df = pd.DataFrame(sample_data)
+df.to_csv('crop_diseases.csv', index=False)
+print("DataFrame created and saved to 'crop_diseases.csv'")
+print(df)
